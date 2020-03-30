8ff53e0b5dfd78a2799a31c620116fefbe810c4b
Andy Pickle <pickleat@gmail.com>
2020-03-30 07:03:33 -0500

    adding className where class had been used previously

--- src/components/FindYourNext.js
+++ src/components/FindYourNext.js
@@ -9,10 +9,10 @@ const FindYourNext = () => {
     }, 2500);
 
     return (
-        <div class='flex justify-center mt-8'>
-            <div class='flex items-center'>
-                <div class='flex-col text-gray-800 font-bold text-4xl'>Find your next</div>
-                <div class="flex flex-col text-gray-800 text-center text-2xl  ml-2 font-mono w-64" style={{borderBottom: '3px solid #54AF8E'}}>{nextWords[word]}</div>
+        <div className='flex justify-center mt-8'>
+            <div className='flex items-center'>
+                <div className='flex-col text-gray-800 font-bold text-4xl'>Find your next</div>
+                <div className="flex flex-col text-gray-800 text-center text-2xl  ml-2 font-mono w-64" style={{borderBottom: '3px solid #54AF8E'}}>{nextWords[word]}</div>
             </div>
         </div>
     )
