be306b09fe5d6d8dde3285612645229863df83cb
Drew Clements <drewclementsdesign@gmail.com>
2020-04-04 13:15:52 -0400

    Form validation and fixed quill editor

--- src/components/QuillInput.js
+++ src/components/QuillInput.js
@@ -5,9 +5,7 @@ import 'react-quill/dist/quill.snow.css';
 function QuillInput() {
 
   return (
-    <ReactQuill>
-      <div className="editor-area" style={{height: 250}}/>
-    </ReactQuill>
+    <ReactQuill/>
   )
 }
 
