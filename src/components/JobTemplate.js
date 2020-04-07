24ca739a5e53aa489909d6c78774802299786450
Andy Pickle <pickleat@gmail.com>
2020-04-07 08:12:04 -0500

    quick and easy way to show the string received from Quilljs inputs

--- src/components/JobTemplate.js
+++ src/components/JobTemplate.js
@@ -3,12 +3,8 @@ import React from 'react'
 
 const JobTemplate = ({props}) => {
 
-  function sanitizeHTML(text){
-    console.log('heres what you get');
-    console.log(text);
-    console.log(typeof(text))
-    console.log(text.length);
-    return text
+  function createMarkup(text){
+    return {__html: text || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
   }
 
   return(
@@ -20,18 +16,14 @@ const JobTemplate = ({props}) => {
 
           <h2 className='text-blue-500 font-bold text-2xl'>{props.jobTitle || 'Position Title'}, {props.companyName || "Company Name"}</h2>
           <div className='text-gray-600 uppercase tracking-tight text-md'>{props.roleFocus} • {props.positionType}</div>
-          <div className='mt-8 text-blue-300 '>
-            
-            {sanitizeHTML(props.jobDescription) || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-    
-          </div>
+          <div className='mt-8 text-blue-300 ' dangerouslySetInnerHTML={createMarkup(props.jobDescription)}></div>
         </div>
         <div className='flex-row w-2/5 bg-gray-200 flex-wrap p-4'>
               <h4 className='text-blue-500 font-bold text-2xl'>About {props.companyName}</h4>
               <div className='text-gray-600 uppercase tracking-tight text-md'>
-                <a href={props.companyWebsite}>Website</a> • <a href={props.companyEmail}>Contact Email</a>
+                <a href={props.companyWebsite}>Website</a> • <a href={`mailto:${props.companyEmail}`}>Contact Email</a>
               </div>
-              <p className='mt-8 text-blue-300'>{props.companyDescription}</p>
+              <div className='mt-8 text-blue-300'dangerouslySetInnerHTML={createMarkup(props.companyDescription)}></div>
         </div>
       </div>
       <div className='flex-row'>
