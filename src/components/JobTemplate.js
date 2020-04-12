e1bd8554ed7224fa95ca412a6ef44b83b74d76a9
Andy Pickle <pickleat@gmail.com>
2020-04-11 20:35:58 -0500

    finishing the formatting coming from QuillJS

--- src/components/JobTemplate.js
+++ src/components/JobTemplate.js
@@ -5,15 +5,14 @@ import { Redirect } from 'react-router-dom'
 const JobTemplate = ({recievingTemplateApproval, props}) => {
   
   const quillStyle = {
-    h1: 'text-blue-500 font-bold text-3xl',
-    h2: 'text-blue-500 font-bold text-2xl',
-    h3: 'text-blue-500 font-bold text-xl',
-    h4: 'text-blue-500 font-bold text-lg', 
+    h1: 'text-blue-500 font-bold text-xl',
+    h2: 'text-blue-500 font-bold text-lg',
+    h3: 'text-blue-500 font-bold text-md',
     a: 'text-teal-600 font-bold',
     p: 'text-blue-300',
     ol: 'list-decimal list-inside',
     ul: 'list-disc list-inside',
-    li: 'text-blue-300'
+    li: 'pl-2 text-blue-300'
   }
 
   useEffect( () => {
@@ -24,12 +23,24 @@ const JobTemplate = ({recievingTemplateApproval, props}) => {
         if(child.tagName === 'H1'){ child.classList = quillStyle.h1 }
         if(child.tagName === 'H2'){ child.classList = quillStyle.h2 }
         if(child.tagName === 'H3'){ child.classList = quillStyle.h3 }
-        if(child.tagName === 'p'){ child.classList = quillStyle.p }
+        if(child.tagName === 'P'){ child.classList = quillStyle.p }
         // below need to be nested loops
-        if(child.tagName === 'a'){ child.classList = quillStyle.a }
-        if(child.tagName === 'ol'){ child.classList = quillStyle.ol }
-        if(child.tagName === 'ul'){ child.classList = quillStyle.ul }
-        if(child.tagName === 'li'){ child.classList = quillStyle.li }
+        if(child.tagName === 'A'){ child.classList = quillStyle.a }
+        if(child.tagName === 'OL'){ 
+          child.classList = quillStyle.ol 
+          let listItems = [...child.children]
+          listItems.forEach(listItem => {
+            listItem.classList = quillStyle.li
+          })
+        }
+        if(child.tagName === 'UL'){ 
+          child.classList = quillStyle.ul 
+          let listItems = [...child.children]
+          listItems.forEach(listItem => {
+            listItem.classList = quillStyle.li
+          })
+        }
+        if(child.tagName === 'LI'){ child.classList = quillStyle.li }
         }
       )
     }
@@ -55,16 +66,19 @@ const JobTemplate = ({recievingTemplateApproval, props}) => {
         {/* <a href='#'>Back to Job List </a> */}
         <div className='w-3/5 pr-2'>
 
-          <h2 className='text-blue-500 font-bold text-2xl'>{props.jobTitle || 'Position Title'}, {props.companyName || "Company Name"}</h2>
-          <div className='text-gray-600 uppercase tracking-tight text-md'>{props.roleFocus} • {props.positionType}</div>
-          <div className='mt-8' id='jobDesc' dangerouslySetInnerHTML={createMarkup(props.jobDescription)}></div>
+          <h2 className='text-blue-500 font-bold text-3xl'>{props.jobTitle || 'Position Title'}, {props.companyName || "Company Name"}</h2>
+          <div className='text-gray-600 uppercase tracking-tight text-md mb-4'>{props.roleFocus} • {props.positionType}</div>
+          <h3 className='text-blue-500 font-bold text-2xl'>Job Description</h3>
+          <div className='mt-2' id='jobDesc' dangerouslySetInnerHTML={createMarkup(props.jobDescription)}></div>
+          <h4 className='text-blue-500 font-bold text-2xl'>About {props.companyName}</h4>
+          <div className='mt-2 text-blue-300' id='companyDesc' dangerouslySetInnerHTML={createMarkup(props.companyDescription)}></div>
+
         </div>
-        <div className='flex-row w-2/5 bg-gray-200 flex-wrap p-4'>
+        <div className='flex-row w-2/5 bg-gray-200 flex-wrap mt-16 p-4 h-64'>
               <h4 className='text-blue-500 font-bold text-2xl flex-wrap'>About {props.companyName}</h4>
               <div className='text-gray-600 uppercase tracking-tight text-md'>
                 <a href={props.companyWebsite}>Website</a> • <a href={`mailto:${props.companyEmail}`}>Contact Email</a>
               </div>
-              <div className='mt-8 text-blue-300' id='companyDesc' dangerouslySetInnerHTML={createMarkup(props.companyDescription)}></div>
         </div>
       </div>
       <div className='flex-row'>
