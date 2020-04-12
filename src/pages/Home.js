2741a367b3578e27faefea5f90eb86a420038a0e
Drew Clements <drewclementsdesign@gmail.com>
2020-04-12 18:32:37 -0400

    more small fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -115,16 +115,16 @@ const tempJobData = [
 const Home = () => (
   <div>
     <img src={heroBG} alt="" className="hidden md:block absolute top-0 left-0 w-full" />
-    <img src={mobileBG} alt="" className="fixed md:hidden w-screen"/>
+    <img src={mobileBG} alt="" className="fixed md:hidden w-full h-full"/>
 
     <div className="relative pt-20 lg:pt-32 px-2">
       <FindYourNext />
       <div className="flex md:w-3/4 flex-col text-center mx-auto mt-6">
-        <p className="tracking-wide font-light lg:w-3/4 mx-auto text-lg text-blue-400 mb-6">
+        <p className="tracking-wide lg:w-3/5 mx-auto text-lg text-blue-400 mb-6">
           Looking for your next junior developer role? Look no further! Any jobs
           listed here are geared for those hungry to work and learn.
         </p>
-        <button className="btn btn-teal mx-auto text-xl">Find A Job</button>
+        <button className="btn btn-teal mx-auto">Find A Job</button>
       </div>
 
       <div className="mt-12 lg:pt-16 mx-auto" style={{ maxWidth: 680 }}>
