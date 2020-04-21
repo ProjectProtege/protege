9fe69471e4335466e4abee36216efc1cdcc22b85
Drew Clements <drewclementsdesign@gmail.com>
2020-04-21 12:50:27 -0400

    should fix quill on dev

--- src/components/QuillInput.js
+++ src/components/QuillInput.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import '../assets/custom-quill.css';
 
 function QuillInput() {
   return (
