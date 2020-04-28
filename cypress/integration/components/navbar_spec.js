803fc45c15b18a4fe7d50cfa733af288b265ca2e
Andy Pickle <pickleat@gmail.com>
2020-04-27 21:36:59 -0500

    adding note about what the test does and does not cover

--- cypress/integration/components/navbar_spec.js
+++ cypress/integration/components/navbar_spec.js
@@ -1,4 +1,5 @@
 describe('navbar test', () => {
+    // tests that every link works, should probably add some tests around which one is showing / hidden at different screensizes
     it('tests the mobile nav', () => {
         cy.visit('/')
         cy.get('[data-cy=mobile-nav]>ul>li>a').should('have.length', 3)
