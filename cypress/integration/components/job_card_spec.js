5350fd4206255eb9946fa0c90d332c2005da3408
Andy Pickle <pickleat@gmail.com>
2020-04-29 09:51:39 -0500

    WIP on the test for the JobCard.js component

--- cypress/integration/components/job_card_spec.js
+++ cypress/integration/components/job_card_spec.js
@@ -5,9 +5,20 @@ describe('job card', () => {
     it('tests the job card', () => {
         cy.get("[data-cy='job-card-container']>a").should('have.length', 6)
             .each( (jobCard) => {
+                beforeEach( () => {
+                    cy.visit('/')
+                })
+                it('tests each individual card')
                 console.log(jobCard)
-                // cy.get('img')
-                // cy.get("[data-cy='job-card-company-name']")
+                // slices specifically to the end of http://localhost:3000/job-board/{jobId}
+                const jobId = jobCard[0].href.slice(32)
+                cy.get(`[data-cy='job-card-image-${jobId}']`).should('have.attr', 'src')
+                cy.get(`[data-cy='job-card-company-name-${jobId}']`)
+                cy.get(`[data-cy='job-card-job-title-${jobId}']`)
+                cy.get(`[data-cy='job-card-role-focus-${jobId}']`)
+                cy.get(`[data-cy='job-card-formatted-date-${jobId}']`)
+                cy.get(`[data-cy='job-card-company-hq-${jobId}']`)
+                cy.get(`[data-cy='job-card-link-${jobId}']`).click()
             })
     })
 })
