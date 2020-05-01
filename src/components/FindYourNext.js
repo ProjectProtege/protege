45bfe88bbc09f2e5c4536a201b0491c8ba8afd11
Drew <drewclementsdesign@gmail.com>
2020-05-01 15:29:38 -0400

    Shorten word rotation time

--- src/components/FindYourNext.js
+++ src/components/FindYourNext.js
@@ -5,7 +5,7 @@ const FindYourNext = () => {
   const [word, setWord] = useState(0)
   setTimeout(() => {
     setWord((word + 1) % nextWords.length)
-  }, 2500)
+  }, 2000)
 
   return (
     <div className='flex justify-center mt-8'>
