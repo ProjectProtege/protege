c21336894dc8716a587007a71c54891d2e8a033e
Drew Clements <drewclementsdesign@gmail.com>
2020-05-05 12:00:10 -0400

    Add thank you page

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import LearningResources from '../pages/learning-resources'
 import GetInTouch from '../pages/get-in-touch'
 import IndividualJobPage from '../pages/individual-job'
 import PostAJob from '../pages/post-a-job'
+import Thanks from '../pages/thanks'
 
 import * as ROUTES from '../constants/routes'
 
@@ -20,6 +21,7 @@ const App = () => (
         <Route path={ROUTES.GET_IN_TOUCH} component={GetInTouch} />
         <Route path={ROUTES.POST_A_JOB} component={PostAJob} />
         <Route path={ROUTES.LEARNING} component={LearningResources} />
+        <Route path={ROUTES.THANKS} component={Thanks} />
       </Switch>
     </Layout>
   </Router>
