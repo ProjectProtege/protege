d0c2b457af57a0f6805ede8cd378e64b43b874b5
Andy Pickle <pickleat@gmail.com>
2020-05-06 08:40:13 -0500

    finishing test for PostAJobform.js with post_a_job_form_spec.js

--- cypress/integration/components/post_a_job_form_spec.js
+++ cypress/integration/components/post_a_job_form_spec.js
@@ -8,14 +8,14 @@ describe('post a job form', () => {
         cy.get('select[name="roleFocus"]')
         cy.get('select[name="positionType"]')
         // Quill Input
-        // cy.get('input[name="jobDescription"]')
+        cy.get(".ql-editor").first().should('have.attr', 'contenteditable')
         cy.get('input[name="howToApply"]')
         cy.get('input[name="companyName"]')
         cy.get('input[name="companyWebsite"]')
         cy.get('input[name="companyEmail"]')
         cy.get('input[name="companyLogo"]')
         // Quill Input
-        // cy.get('input[name="companyDescription"]')
+        cy.get(".ql-editor").last().should('have.attr', 'contenteditable')
         cy.get('input[name="companyHQ"]')
     })
     it('tests the form for error messages', () => {
@@ -24,18 +24,27 @@ describe('post a job form', () => {
     })
     it('tests inputs all the form fields', () => {
         cy.get('input[name="jobTitle"]').type('Junior Developer')
-        cy.get('select[name="roleFocus"]')
-        cy.get('select[name="positionType"]')
+        cy.get('select[name="roleFocus"]').select('Front-end')
+        cy.get('select[name="positionType"]').select('Full-time')
         // Quill Input
-        // cy.get('input[name="jobDescription"]')
-        cy.get('input[name="howToApply"]').type('burtMaclin@fbi.com')
-        cy.get('input[name="companyName"]').type('FBI')
-        cy.get('input[name="companyWebsite"]').type('FBI.gov') 
-        cy.get('input[name="companyEmail"]').type('burtMaclin@fbi.com')
-        cy.get('input[name="companyLogo"]')
+        cy.get(".ql-editor").first().type('This is a {cmd}b GREAT gig.')
+        cy.get('input[name="howToApply"]').type('tom@snakeholelounge.club')
+        cy.get('input[name="companyName"]').type('Snakehole Lounge')
+        cy.get('input[name="companyWebsite"]').type('https://snakeholelounge.club') 
+        cy.get('input[name="companyEmail"]').type('tom@snakeholelounge.club')
+        
+        // Logo Upload
+        cy.fixture('SnakeholeLoungeLogo.png').then( fileContent => {
+            cy.get("[data-cy='company-logo-upload']").upload(
+                { fileContent, fileName: 'SnakeholeLoungeLogo.png', mimeType: 'image/*' },
+                { subjectType: 'input' }
+            )
+            cy.get("[data-cy='company-logo-uploaded']").should('have.attr', 'src')
+            cy.get("[data-cy='logo-upload-fileName']").contains('SnakeholeLoungeLogo.png')
+        })
         // Quill Input
-        // cy.get('input[name="companyDescription"]')
-        cy.get('input[name="companyHQ"]').type('Washington, D.C.')
+        cy.get(".ql-editor").last().type('We are the dopest, trillest club around.')
+        cy.get('input[name="companyHQ"]').type('Pawnee, IN')
         cy.get("[data-cy='next-step-button']").click()
     })
 
