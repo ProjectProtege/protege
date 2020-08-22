e80a6dc093c182055835d7cf5f466e22845bfdd9
Andy Pickle <pickleat@gmail.com>
2020-08-22 14:12:16 -0500

    fixing cursor-pointer

--- src/components/form/TierSelectCard.js
+++ src/components/form/TierSelectCard.js
@@ -6,10 +6,10 @@ const TierSelectCard = ({ children, value, receivingTierClick, tier }) => {
 
   return (
     <div
-      className={`p-6 md:p-8 shadow rounded text-left md:text-center border-t-8 border-gray-300  transform ease-in-out duration-150 bg-white mx-auto mb-4 grid grid-cols-3 md:grid-cols-1 w-full
+      className={`p-6 md:p-8 shadow rounded text-left md:text-center cursor-pointer border-t-8 border-gray-300  transform ease-in-out duration-150 bg-white mx-auto mb-4 grid grid-cols-3 md:grid-cols-1 w-full
       ${
         route.pathname === '/post-a-job'
-          ? 'hover:border-teal-300 hover:scale-107 hover:shadow-md cursor-pointer'
+          ? 'hover:border-teal-300 hover:scale-107 hover:shadow-md'
           : ''
       }
       ${tier === value ? 'border-teal-300 bg-gray-100 scale-105' : ''}`}
