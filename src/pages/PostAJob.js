5a1275da17dbf1564d50ba60dc4e2e2faf47fe48
Drew <drewclementsdesign@gmail.com>
2020-09-07 15:11:34 -0400

    listing verbiage

--- src/pages/PostAJob.js
+++ src/pages/PostAJob.js
@@ -11,6 +11,7 @@ import StatusBar from '../components/form/StatusBar'
 import JobTemplate from '../components/JobTemplate'
 import JobPostingConfirmation from '../components/JobPostingConfirmation'
 import TierSelect from '../components/form/TierSelect'
+import Check from '../assets/images/check-solid.js'
 
 //firebase
 import { db, storage } from '../firebase/firebase'
@@ -130,6 +131,51 @@ const PostAJob = ({ location }) => {
       >
         {status === 1 && (
           <>
+            <div className='container mx-auto lg:w-4/5 mb-12 flex flex-col items-center'>
+              <h3 className='text-xl font-bold text-blue-900 mb-3'>
+                What qualifies as a junior remote job opportunity on Protegé?
+              </h3>
+
+              <p className='text-blue-700 text-sm lg:text-base lg:leading-relaxed mb-4 lg:text-center'>
+                Our mission is to help those early in their tech career find
+                their next opporunities to thrive. Below is a list we've
+                provided to help you determine if the role you're hiring for
+                fits within our requirements here at Protegé.
+              </p>
+
+              <ul className='leading-loose text-blue-800 mb-3 text-sm lg:text-base'>
+                <li>
+                  <span className='text-teal-600 absolute w-5 h-5 lg:mt-1'>
+                    <Check />
+                  </span>
+                  <p className='pl-8 lg:pl-10'>The job must be fully remote.</p>
+                </li>
+                <li>
+                  <span className='text-teal-600 absolute w-5 h-5 lg:mt-1'>
+                    <Check />
+                  </span>
+                  <p className='pl-8 lg:pl-10'>
+                    Do not require 3 (or more) years experience.
+                  </p>
+                </li>
+                <li>
+                  <span className='text-teal-600 absolute w-5 h-5 lg:mt-1'>
+                    <Check />
+                  </span>
+                  <p className='pl-8 lg:pl-10'>
+                    Do not require a 4 year degree (or equivalent experience).
+                  </p>
+                </li>
+              </ul>
+
+              <p className='text-xs text-blue-600 lg:text-center lg:w-3/4 opacity-75'>
+                Protegé.dev is a curated job board tailored towards junior
+                developers. Each listing is reviewed, and approved or denied
+                before going live. If your listing is denied, we'll contact you
+                through email with suggested edits.
+              </p>
+            </div>
+
             <TierSelect receivingTierClick={receivingTierClick} tier={tier} />
 
             <p className={`text-center mb-2 text-blue-400 tracking-wide`}>
