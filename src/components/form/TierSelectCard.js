a43729feec6f6a0f53d45f30615f1c3ecce34acc
Drew <drewclementsdesign@gmail.com>
2020-09-19 00:04:08 -0400

    tier select card eslint errors

--- src/components/form/TierSelectCard.js
+++ src/components/form/TierSelectCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 const TierSelectCard = ({ children, value, receivingTierClick, tier }) => {
   const route = useLocation()
@@ -13,9 +14,14 @@ const TierSelectCard = ({ children, value, receivingTierClick, tier }) => {
           : ''
       }
       ${tier === value ? 'border-teal-300 bg-gray-100 scale-105' : ''}`}
-      onClick={(e) => {
+      onClick={() => {
         receivingTierClick(value)
       }}
+      onKeyDown={() => {
+        receivingTierClick(value)
+      }}
+      role='button'
+      tabIndex='0'
     >
       {route.pathname === '/' ? (
         <div className='hidden absolute bg-error px-4 py-2 z-10 text-white uppercase -rotate-15 transform text-sm font-semibold tracking-widest -ml-1 shadow'>
@@ -27,6 +33,17 @@ const TierSelectCard = ({ children, value, receivingTierClick, tier }) => {
       {children}
     </div>
   )
+}
+
+TierSelectCard.propTypes = {
+  children: PropTypes.node.isRequired,
+  value: PropTypes.string.isRequired,
+  receivingTierClick: PropTypes.func.isRequired,
+  tier: PropTypes.string,
+}
+
+TierSelectCard.defaultProps = {
+  tier: '',
 }
 
 export default TierSelectCard
