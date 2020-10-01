6c071c9b72ea0a755c7625f175a7d2fd7818ca6e
andregamma <toeoficial@outlook.com>
2020-10-01 13:14:44 -0400

    Fix accessibility issues on footer

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -44,15 +44,15 @@ const Footer = () => {
         },
       }}
     >
-      <div data-cy='footer' className='bg-gray-200 py-4 px-3 md:px-0 mb-6'>
+      <div data-cy='footer' className='px-3 py-4 mb-6 bg-gray-200 md:px-0'>
         <form
           className='container mx-auto'
           name='email-list'
           onSubmit={subscribeEmail}
         >
-          <div className='flex flex-col md:flex-row justify-center items-center'>
+          <div className='flex flex-col items-center justify-center md:flex-row'>
             <label
-              className='text-blue-900 text-lg md:mr-6 font-semibold text-center md:text-left'
+              className='text-lg font-semibold text-center text-blue-900 md:mr-6 md:text-left'
               htmlFor='newsletter-email'
             >
               Sign up for our mailing list!
@@ -62,7 +62,7 @@ const Footer = () => {
               value={userEmail}
               data-cy='mailing-list-signup'
               type='email'
-              className='px-4 py-1 w-full md:w-2/5 my-3 md:my-0'
+              className='w-full px-4 py-1 my-3 md:w-2/5 md:my-0'
               id='newsletter-email'
               autoComplete='off'
               onChange={(e) => setUserEmail(e.target.value)}
@@ -73,28 +73,28 @@ const Footer = () => {
             <button
               data-cy='mailing-list-signup-button'
               type='submit'
-              className='btn btn-blue w-full md:w-auto'
+              className='w-full btn btn-blue md:w-auto'
             >
               Sign Up
             </button>
           </div>
 
-          <p className='text-center text-sm mt-3 text-blue-700'>
+          <p className='mt-3 text-sm text-center text-blue-700'>
             Stay up to date on improvements to Protege.dev and when new
             opportunities arrive!
           </p>
         </form>
       </div>
 
-      <div className='container mx-auto px-8'>
+      <div className='container px-8 mx-auto'>
         <div className='flex justify-between'>
           <GrayLogo />
 
           <div data-cy='footer-links' className='flex flex-col items-end'>
-            <ul className='text-right mb-3'>
+            <ul className='mb-3 text-right'>
               <li className='mb-1'>
                 <Link
-                  className='text-blue-800 opacity-75 hover:opacity-100'
+                  className='text-blue-900 opacity-75 hover:opacity-100'
                   to={ROUTES.JOB_BOARD}
                 >
                   Find a Job
@@ -103,7 +103,7 @@ const Footer = () => {
 
               <li className='mb-1'>
                 <Link
-                  className='text-blue-800 opacity-75 hover:opacity-100'
+                  className='text-blue-900 opacity-75 hover:opacity-100'
                   to={ROUTES.LEARNING}
                 >
                   Learning Resources
@@ -112,7 +112,7 @@ const Footer = () => {
 
               <li>
                 <Link
-                  className='text-blue-800 opacity-75 hover:opacity-100'
+                  className='text-blue-900 opacity-75 hover:opacity-100'
                   to={ROUTES.POST_A_JOB}
                 >
                   Post a Job
@@ -120,7 +120,7 @@ const Footer = () => {
               </li>
               <li>
                 <a
-                  className='text-blue-800 opacity-75 hover:opacity-100'
+                  className='text-blue-900 opacity-75 hover:opacity-100'
                   href='https://github.com/drewclem/protege/issues'
                   target='_blank'
                   rel='noopener noreferrer'
@@ -132,7 +132,7 @@ const Footer = () => {
 
             <a
               href='https://twitter.com/devprotege'
-              className='text-teal-300 hover:text-teal-600 transition-colors ease-in-out duration-75 w-6'
+              className='w-6 text-teal-300 transition-colors duration-75 ease-in-out hover:text-teal-600'
               target='_blank'
               rel='noopener noreferrer'
               aria-label='Follow Protege on Twitter'
@@ -143,13 +143,14 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='flex flex-col  items-center lg:flex-row justify-center tracking-wide m-6'>
-        <p className='lg:mr-12 mb-4 lg:mb-0 text-blue-600'>
+      <div className='flex flex-col items-center justify-center m-6 tracking-wide lg:flex-row'>
+        <p className='mb-4 text-blue-700 lg:mr-12 lg:mb-0'>
           Copyright 2020 © Protege.dev
         </p>
         <a
           href='https://stripe.com'
-          className='text-blue-300 hover:text-blue-900 transition ease-in-out duration-150 -mt-1'
+          className='-mt-1 text-blue-300 transition duration-150 ease-in-out hover:text-blue-900'
+          aria-label='Stripe'
         >
           <StripeIcon className='text-blue-200' />
         </a>
