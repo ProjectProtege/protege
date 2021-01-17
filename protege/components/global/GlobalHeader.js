3aaee008b7c4f33bc774526a71b0b78668e011bc
Drew <drewclementsdesign@gmail.com>
2021-01-17 12:31:31 -0500

    fix mobile menu on tablet

--- protege/components/global/GlobalHeader.js
+++ protege/components/global/GlobalHeader.js
@@ -10,7 +10,7 @@ const GlobalHeader = () => {
 
   return (
     <header className='relative container flex justify-between items-center py-6 px-6 xl:px-0'>
-      <Link href='/' className='w-1/4 md:w-1/6 h-auto'>
+      <Link href='/' className='w-1/4 lg:w-1/6 h-auto'>
         <a>
           <span>
             <Logo className='w-full' />
@@ -19,7 +19,7 @@ const GlobalHeader = () => {
       </Link>
 
       <button
-        className='absolute right-0  mr-4 md:hidden h-12 w-12 text-blue-900  p-2'
+        className='absolute right-0  mr-4 lg:hidden h-12 w-12 text-blue-900  p-2'
         aria-label='navigation'
         onClick={() => setActive((oldActive) => !oldActive)}
         type='button'
@@ -31,10 +31,10 @@ const GlobalHeader = () => {
         data-cy='mobile-nav'
         className={`${
           active ? 'translate-0' : 'translate-x-full'
-        } absolute w-screen inset-0 flex transform transition duration-150 ease-in-out`}
+        } lg:hidden absolute w-screen max-w-md top-0 right-0 flex flex-row transform transition duration-150 ease-in-out`}
       >
         <button
-          className='md:hidden h-12 w-12 text-blue-900 bg-white rounded-full p-1 shadow-md ml-4 mt-8'
+          className='lg:hidden h-12 w-12 text-blue-900 bg-white rounded-full p-1 shadow-md ml-2 mt-8'
           aria-label='navigation'
           onClick={() => setActive((oldActive) => !oldActive)}
           type='button'
@@ -42,7 +42,7 @@ const GlobalHeader = () => {
           <CloseIcon />
         </button>
 
-        <ul className='bg-white w-full h-screen shadow-xl ml-4'>
+        <ul className='bg-white w-full h-screen shadow-xl ml-2'>
           <li className='py-4 px-6 border-b border-gray-300'>
             <NavLink
               href='/job-board'
@@ -83,7 +83,7 @@ const GlobalHeader = () => {
               Contributors
             </NavLink>
           </li>
-          <li className='bg-teal-300 text-blue-900 ml-6 px-3 w-1/2 text-center py-2 mt-4'>
+          <li className='bg-teal-300 text-blue-900 ml-6 px-3 w-1/2 text-center py-2 mt-4 font-bold uppercase'>
             <NavLink href='/'>Post a Job</NavLink>
           </li>
         </ul>
@@ -91,7 +91,7 @@ const GlobalHeader = () => {
 
       <nav
         data-cy='desktop-nav'
-        className='hidden md:block text-blue-900 text-sm uppercase font-medium'
+        className='hidden lg:block text-blue-900 text-sm uppercase font-medium'
         role='navigation'
       >
         <ul className='flex justify-between'>
@@ -101,7 +101,7 @@ const GlobalHeader = () => {
           >
             <NavLink
               href='/job-board'
-              className='opacity-75 pb-3 hover:opacity-100 border-b border-transparent'
+              className='opacity-75 pb-3 hover:opacity-100 border-b-2 border-transparent'
               activeClassName='border-teal-500'
             >
               Find a Job
@@ -141,7 +141,7 @@ const GlobalHeader = () => {
           <li className='menu-item mt-1 pr-4 lg:pr-10 opacity-75 hover:opacity-100'>
             <NavLink
               href='/learning-resources'
-              className='pb-3 border-b transition-colors duration-75 hover:border-teal-500 '
+              className='pb-3 border-b-2 transition-colors duration-75 hover:border-teal-500 '
               activeClassName='border-teal-500'
               inactiveClassName='border-transparent'
             >
@@ -152,7 +152,7 @@ const GlobalHeader = () => {
           <li className='menu-item mt-1 pr-4 lg:pr-10 opacity-75 hover:opacity-100'>
             <NavLink
               href='/get-in-touch'
-              className='pb-3 border-b transition-colors duration-75 hover:border-teal-500 '
+              className='pb-3 border-b-2 transition-colors duration-75 hover:border-teal-500 '
               activeClassName='border-teal-500'
               inactiveClassName='border-transparent'
             >
@@ -163,7 +163,7 @@ const GlobalHeader = () => {
           <li className='menu-item mt-1 pr-4 lg:pr-10 opacity-75 hover:opacity-100'>
             <NavLink
               href='/contributors'
-              className='pb-3 border-b transition-colors duration-75 hover:border-teal-500 '
+              className='pb-3 border-b-2 transition-colors duration-75 hover:border-teal-500 '
               activeClassName='border-teal-500'
               inactiveClassName='border-transparent'
             >
