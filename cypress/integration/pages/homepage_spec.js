a3e5a9ce09c53e14fff474d780a7f66c0f682c5f
Drew <drewclementsdesign@gmail.com>
2021-02-20 21:32:40 -0500

    the big push

--- cypress/integration/pages/homepage_spec.js
+++
@@ -1,8 +0,0 @@
-const pageTitle = ''
-const stringToTest = 'Latest Opportunities'
-describe(`homepage test`, () => {
-  it(`tests that the homepage exists`, () => {
-    cy.visit(`/${pageTitle}`)
-    cy.contains(stringToTest)
-  })
-})
--- cypress/integration/pages/post_a_job_spec.js
+++
@@ -1,8 +0,0 @@
-const pageTitle = 'post-a-job'
-const stringToTest = 'Inexperienced doesn’t mean incapable.'
-describe(`${pageTitle} test`, () => {
-  it(`tests that the ${pageTitle} page exists`, () => {
-    cy.visit(`/${pageTitle}`)
-    cy.contains(stringToTest)
-  })
-})
