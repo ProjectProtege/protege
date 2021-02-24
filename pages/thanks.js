9ecd0a061b739bb361196d780502408509ce9230
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2021-02-24 15:28:24 -0500

    Contact Form - Integrates From Spree to deploy to Vercel
    - front end validation with RHF & Yup
    - adds axios

--- pages/thanks.js
+++ pages/thanks.js
@@ -3,9 +3,9 @@ import Link from 'next/link'
 const Thanks = () => {
   return (
     <div className='container' style={{ maxWidth: 680 }}>
-      <h1 className='text-2xl text-blue-500 mb-3'>Thanks for your comment!</h1>
+      <h1 className='mb-3 text-2xl text-blue-500'>Thanks for your comment!</h1>
 
-      <p className='text-blue-200 mb-12'>
+      <p className='mb-12'>
         We appreciate your input and the team will review it soon.
         <br />
         <br />
