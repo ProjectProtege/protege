a05ac769141feb358fd4f5ab5b8dffd8eea5be17
Drew <drewclementsdesign@gmail.com>
2021-03-02 23:04:10 -0500

    gradient all the things

--- components/form/TierSelectCard.js
+++ components/form/TierSelectCard.js
@@ -13,7 +13,7 @@ const TierSelectCard = ({ children, value }) => {
 
   return (
     <button
-      className={`p-6 md:p-8 shadow rounded text-left md:text-center cursor-pointer border-t-8 border-gray-300  transform ease-in-out duration-150 bg-white mx-auto mb-4 grid grid-cols-3 md:grid-cols-1 w-full
+      className={`overflow-hidden p-6 md:p-8 shadow rounded text-left md:text-center cursor-pointer transform ease-in-out duration-150 bg-white mx-auto mb-4 grid grid-cols-3 md:grid-cols-1 w-full
       ${
         route.route === '/post-a-job'
           ? 'hover:border-teal-300 hover:scale-107 hover:shadow-md'
@@ -23,6 +23,11 @@ const TierSelectCard = ({ children, value }) => {
       onClick={handleClick}
       type='button'
     >
+      <div
+        className={`absolute w-full h-2 bg-gradient-to-r from-gray-400 to-gray-300 ${
+          tier === value ? 'from-teal-500 to-teal-300' : ''
+        }`}
+      ></div>
       {route.route === '/' ? (
         <div className='hidden absolute bg-error px-4 py-2 z-10 text-white uppercase -rotate-15 transform text-sm font-semibold tracking-widest -ml-1 shadow'>
           Free for now!
