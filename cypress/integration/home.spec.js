b92680dc4f562f9bccad672c2315ec4c3b740bc8
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2021-03-09 13:54:20 -0500

    Successfully tests Cypress, adds 'Post a Job' test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,3 @@
-import { it } from 'mocha'
-
 context('Home page', () => {
   beforeEach(() => {
     cy.visit('/')
@@ -8,11 +6,14 @@ context('Home page', () => {
   it('renders', () => {
     // Check for the screen reader h1
     cy.get('h1')
-      .should('have.text', 'Protege.dev | Remote jobs for junior developers')
       .should('exist')
+      .should('have.text', 'Protege.dev | Remote jobs for junior developers')
 
     // Check that links to the job board exist
-    cy.findAllByRole('link', { name: 'Find a Job' }).should('exist')
+    cy.findAllByRole('link', { name: 'Find a Job' }).should('exist').should('have.length', 3)
+
+    cy.findAllByRole('link', {name: 'Post a Job'}).should('exist').should('have.length', 3)
+
     // Check that the email sign up button exist
     cy.findByRole('button', { name: 'Sign Up' }).should('exist')
   })
