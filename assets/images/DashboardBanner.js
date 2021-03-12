ed6dec80110338dd2be3d961b2bf00aae1cecd86
Drew <drewclementsdesign@gmail.com>
2021-03-11 23:16:38 -0500

    darker gradient

--- assets/images/DashboardBanner.js
+++ assets/images/DashboardBanner.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const DashboardBanner = () => {
   return (
-    <div className='absolute z-0 w-full h-12 overflow-hidden lg:h-32 bg-gradient-to-r from-teal-600 to-teal-100'>
+    <div className='absolute z-0 w-full h-12 overflow-hidden lg:h-32 bg-gradient-to-r from-teal-700 to-teal-300'>
       <div style={{ opacity: '.05' }}>
         <Image
           src='/images/pattern-1.png'
