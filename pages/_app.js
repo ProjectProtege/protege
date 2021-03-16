945a3a4252560836ad24fbce8b604503407393ea
Drew <drewclementsdesign@gmail.com>
2021-03-16 00:07:41 -0400

    small stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-
+import Head from 'next/head'
 import GlobalLayout from 'layouts/GlobalLayout'
 import 'assets/styles/globals.css'
 import { useEffect } from 'react'
@@ -53,9 +53,15 @@ function MyApp({ Component, pageProps }) {
   }, [])
 
   return (
-    <GlobalLayout>
-      <Component {...pageProps} />
-    </GlobalLayout>
+    <>
+      <Head>
+        <link rel='shortcut icon' href='/protege-logo.png'></link>
+      </Head>
+
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
+    </>
   )
 }
 
