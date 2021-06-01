043f61aaf0a2a41be007a6e2ca4322131eb05c88
courtney l <clandau@learnics.com>
2021-06-01 13:02:42 -0400

    add required company profile fields to store

--- store/profile_info.js
+++ store/profile_info.js
@@ -3,6 +3,21 @@ import create from 'zustand'
 export const useProfileInfo = create((set) => ({
   profileInfo: null,
   postedJobs: null,
+  requiredCompanyProfileFields: [
+    'displayName',
+    'accountType',
+    'slug',
+    'email',
+    'userUid',
+    'companyWebsite',
+    'companyEmail',
+    'companyHQ',
+    'companyTimezone',
+    'companyTimeframeFrom',
+    'companyTimeframeTo',
+    'companyDescription',
+    'companyLogo',
+  ],
   setProfileInfo: (profileInfo) => {
     return set({ profileInfo })
   },
