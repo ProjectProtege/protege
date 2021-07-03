a2a5dd7b02359fd8676467b9cc6c9e6750d25755
courtney l <clandau@learnics.com>
2021-07-03 10:57:02 -0400

    dont throw an error when verifyIdToken returns an expired error.

--- utils/db/firebaseAdmin.js
+++ utils/db/firebaseAdmin.js
@@ -1,18 +1,19 @@
 const admin = require('firebase-admin')
 const serviceAccount = require('./serviceAccountKey')
 
-export const verifyIdToken = (token) => {
+export const verifyIdToken = async (token) => {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
       databaseURL: 'https://protege-dev-env.firebaseio.com',
     })
   }
-
-  return admin
-    .auth()
-    .verifyIdToken(token)
-    .catch((err) => {
+  try {
+    await admin.auth().verifyIdToken(token)
+  } catch (err) {
+    // if the token is expired, don't throw an error, it will be refreshed in AuthContext
+    if (err.errorInfo.code !== 'auth/id-token-expired') {
       throw err
-    })
+    }
+  }
 }
