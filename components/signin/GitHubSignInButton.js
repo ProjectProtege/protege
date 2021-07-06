923039be74a200ca29431715989e06d6b1109db3
agatakolohe <agatakolohe@gmail.com>
2021-07-05 20:46:07 -0700

    remove async from useEffect

--- components/signin/GitHubSignInButton.js
+++ components/signin/GitHubSignInButton.js
@@ -2,25 +2,29 @@ import firebase from 'firebase/app'
 import 'firebaseui/dist/firebaseui.css'
 import { useEffect } from 'react'
 
+async function githubLogin() {
+  const firebaseui = await import('firebaseui')
+  const uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      {
+        provider: firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        signInMethod: firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      },
+    ],
+  }
+  if (firebaseui.auth.AuthUI.getInstance()) {
+    const ui = firebaseui.auth.AuthUI.getInstance()
+    ui.start('#firebaseui-auth-container', uiConfig)
+  } else {
+    const ui = new firebaseui.auth.AuthUI(firebase.auth())
+    ui.start('#firebaseui-auth-container', uiConfig)
+  }
+}
+
 const GitHubSignInButton = () => {
-  useEffect(async () => {
-    const firebaseui = await import('firebaseui')
-    const uiConfig = {
-      signInFlow: 'popup',
-      signInOptions: [
-        {
-          provider: firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          signInMethod: firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        },
-      ],
-    }
-    if (firebaseui.auth.AuthUI.getInstance()) {
-      const ui = firebaseui.auth.AuthUI.getInstance()
-      ui.start('#firebaseui-auth-container', uiConfig)
-    } else {
-      const ui = new firebaseui.auth.AuthUI(firebase.auth())
-      ui.start('#firebaseui-auth-container', uiConfig)
-    }
+  useEffect(() => {
+    githubLogin()
   })
 
   return <div id='firebaseui-auth-container' />
