5bd8c39fdd7ce953a455eaf222bac1cd91a7e3ee
courtney l <clandau@learnics.com>
2021-07-07 19:53:31 -0400

    remove unneccesary async keyword

--- utils/db/firebaseAdmin.js
+++ utils/db/firebaseAdmin.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin')
 const serviceAccount = require('./serviceAccountKey')
 
-export const verifyIdToken = async (token) => {
+export const verifyIdToken = (token) => {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
