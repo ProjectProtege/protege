cecb6403132c487a1805a38f0d011f75d26b635e
Drew <drewclementsdesign@gmail.com>
2021-07-25 22:08:24 -0400

    adjust button width

--- components/dashboard/VerifyEmail.js
+++ components/dashboard/VerifyEmail.js
@@ -4,21 +4,23 @@ const VerifyEmail = () => {
   return (
     <div className='grid md:grid-cols-2 gap-12 items-center mb-24 md:mb-0'>
       <Unverified className='max-h-[50vh]' />
-      <div className='flex flex-col space-y-6'>
+      <div className='flex flex-col space-y-6 md:mb-10'>
         <h2 className='text-2xl leading-tight'>
           Verify your email before getting started!
         </h2>
+
         <p className='text-blue-700'>
           Check your email and click the link we sent to verify your email and
           unlock your account!
         </p>
+
         <p className='text-blue-700'>
           Need another one? Don&apos;t see the first one?{' '}
-          <button className='underline text-teal-700 font-bold' type='button'>
-            Click here
-          </button>{' '}
-          to have a fresh one sent to you!
         </p>
+
+        <button className='btn btn-teal md:w-1/2' type='button'>
+          Resend Verification
+        </button>
       </div>
     </div>
   )
