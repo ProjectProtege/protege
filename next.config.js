97d4e1c978484eacfc45b12cbf0e7e6353ae11dc
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-10-10 23:33:59 -0600

    Reenable eslint on build command
    Fix for issue #325

--- next.config.js
+++ next.config.js
@@ -35,10 +35,5 @@ module.exports = {
     FB_TOKEN_URI: process.env.FB_TOKEN_URI,
     FB_AUTH_PROVIDER_X509_CERT_URL: process.env.FB_AUTH_PROVIDER_X509_CERT_URL,
     FB_CLIENT_X509_CERT_URL: process.env.FB_CLIENT_X509_CERT_URL,
-  },
-  eslint: {
-    // Warning: Dangerously allow production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
+        }
+  }
